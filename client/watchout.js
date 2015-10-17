58daf288c0abc51a6334c1cd377f7ba2e64efb59
DhiMalo <aherisan@gmail.com>
2015-10-16 20:24:35 -0700

    Enemy Class Added

--- client/watchout.js
+++ client/watchout.js
@@ -7,6 +7,13 @@
 //   height: 300,  
 
 // };
+// var enemy = [{
+//   x: Math.floor(Math.random() * 600),
+//   y: Math.floor(Math.random() * 400)
+// }];
+
+var x = Math.floor(Math.random() * 600);
+var y = Math.floor(Math.random() * 400);
 
 var player = {
 
@@ -18,20 +25,30 @@ var player = {
  //Create some enemies w/ an enemy class
      //define the shape of the enemy
     //add color attribute
- var enemy = d3.select("body").append("enemy")
-  .attr("border-radius", 10 + "px")
-  .attr("border", 10 + "px" + "solid purple")
+
+var svg = d3.select("body").append("svg")
   .append("g")
-  .attr("transform", "translate(" +x+ ","+y+ ")");
+  .attr("x", x)
+  .attr("y", y)
+  .attr("transform", "translate(" + x + "," + y + ")");
+
+ var createEnemyNode = d3.select("span").data("svg")
+  .enter()
+  .append("span")
+  .attr("class", "enemy")
+  .attr("border-radius", 1000 + "px")
+  .attr("border", 1000 + "px")
+  .attr("border-color", "white");
+  // .attr("color", white);
 
  //Create a function where enemies move randomly (setInterval maybe) and have their coordinates "TRANSFORMED"
- var moveEnemy = function(x,y){
-  var randX = Math.floor(Math.random() * 600);
-  var randY = Math.floor(Math.random() * 400);
-  return randX, randY;
- };
+ // var moveEnemy = function(x,y){
+ //  var randX = Math.floor(Math.random() * 600);
+ //  var randY = Math.floor(Math.random() * 400);
+ //  return randX, randY;
+ // };
 
- setInterval(moveEnemy, 1000);
+ // setInterval(moveEnemy, 1000);
  
  //Create the score, set to zero
  var score = 0;
@@ -40,8 +57,8 @@ var player = {
     //Increment the score every 1/10 ms player is still alive
 
   //Else if (player's coordinates === any enemy coordinates){
-    score is reset
-  }
+  //   score is reset
+  // }
 
   //create a click handler to select the player to move its x, y coordinates while clicked
     //if no longer clicked release player 
